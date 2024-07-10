ccef43202d7acf64183fe0c9823ce2bb7ce3fc76
kwabounga <jeanyves.chaillou@gmail.com>
2024-07-10 07:32:26 +0200

    ground size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from './local-storage.service';
+
 const LOCAL_STORAGE_KEY:string = 'fez_codex_state';
 const GLYPH_DEFAULT_VALUE:number = -1;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
+
   currentMouseX = 0;
   currentMouseY = 0;
-  constructor(private localStorageService: LocalStorageService) {
-    document.addEventListener('scroll', (event:any) => {
-      this.scrollY = (window.scrollY);
-    })
-    this.animationTics();
-    let self = this;
-    window.addEventListener('mousemove', function (e) {
-      self.currentMouseX = e.pageX;
-      self.currentMouseY = e.pageY;
-  }, /*useCapture=*/true);
-  }
   scrollY = 0;
   uiHidden:boolean = true;
   currentGlyph:number = GLYPH_DEFAULT_VALUE;
@@ -28,6 +20,21 @@ export class AppComponent {
   currentTile:string = "00";
   currentMessage:number = 0;
   title = 'fez_codex';
+
+  constructor(private localStorageService: LocalStorageService) {
+    document.addEventListener('scroll', (event:any) => {
+      this.scrollY = (window.scrollY);
+    })
+
+    this.animationTics();
+    let self = this;
+
+    window.addEventListener('mousemove', function (e) {
+      e.preventDefault();
+      self.currentMouseX = e.pageX;
+      self.currentMouseY = e.pageY;
+    }, true);
+  }
 
   mapping: any = {
     "00":" ",
@@ -68,35 +75,7 @@ export class AppComponent {
     "35":"?",
   }
   symbols = Object.keys(this.mapping);
-  messages:any = [
-    /* [
-      "01","07","02","01","00","03","02",
-      "01","00","00","26","00","00","01",
-      "01","07","02","01","00","03","01",
-      "01","07","00","01","05","03","05",
-      "01","00","02","01","05","03","08",
-      "00","00","00","00","05","03","09",
-      "00","07","02","01","05","03","00",
-      "01","07","02","01","05","03","04",
-      "00","07","02","00","05","03","05",
-      "01","00","00","01","05","03","35",
-      "01","07","02","01","05","05","05","05",
-    ],
-    [
-      "02","07","02","01","00","03","02",
-      "01","00","00","26","00","00","01",
-      "01","07","02","01","00","03","01",
-      "01","07","00","01","05","03","05",
-      "01","00","02","01","05","03","08",
-      "00","00","00","00","05","03","09",
-      "00","07","02","01","05","03","00",
-      "01","07","02","01","05","03","04",
-      "00","07","02","00","05","03","05",
-      "01","00","00","01","05","03","35",
-      "01","07","02","01","05","05","05","16",
-    ], */
-  ];
-
+  messages:any = [ ];
   tiles:string[] = [
     "01","07","02","01","00","03","02",
     "01","00","00","26","00","00","01",
@@ -120,14 +99,17 @@ export class AppComponent {
       this.uiHidden = save.uiHidden??true;
     }
   }
+
   razMessage(event:any){
     console.log('razMessage',event)
     this.tiles = new Array(78).fill('00');
   }
+
   copieMessage(_msgId:number){
     console.log('copieMessage')
     return Object.create(this.messages[_msgId]);
   }
+
   saveMessage(){
     console.log('saveMessage')
     this.messages.push(Object.create(this.tiles));
@@ -139,15 +121,18 @@ export class AppComponent {
     console.log("glyphId ", glyphId)
     this.currentGlyph = glyphId
   }
+
   onSelectGlyphFromAnticKeyBoard(glyphID: any) {
     this.tiles[this.currentGlyph] = glyphID;
     this.currentGlyph = GLYPH_DEFAULT_VALUE;
   }
+
   // codex & keyboard part
   assignLetter(tileId: string) {
     console.log("tileId", tileId)
     this.currentTile = tileId
   }
+
   onSelectLetterFromKeyBoard(letter: any) {
     console.log("letter", letter);
     if(this.currentTile != "00"){
@@ -155,6 +140,7 @@ export class AppComponent {
     }
     this.currentTile = "00";
   }
+
   toggleUi(){
     this.uiHidden = !this.uiHidden;
   }
@@ -173,9 +159,11 @@ export class AppComponent {
     const value = this.localStorageService.getItem(LOCAL_STORAGE_KEY);
     return value ? JSON.parse(value) : null;
   }
+
   setDevice(device:any){
     console.log("device", device);
   }
+
   animationTics(){
     const step = () => {
       // set mouse positions
